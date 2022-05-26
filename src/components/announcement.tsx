59f18eb32732fc40d8937edbf833a17f4b47c689
Robert <contentmediaguy@gmail.com>
2022-05-26 00:08:15 -0400

    initial modifications with new repo

---
+++ src/components/announcement.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Announcement() {
+    
+    
+    return (
+        <div className="bg-image">
+            <section id="announcement">
+            
+
+            </section>
+        </div>
+    );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <section id="navbar">
+            <ul>
+                <li className="link"><a href="#">menu</a></li>
+                <li><a href="#"><img src="https://user-images.githubusercontent.com/65358991/170405348-83059607-8530-43bf-9887-6a175293109c.png" alt="logo" /></a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+        </section>
+    )
+}
