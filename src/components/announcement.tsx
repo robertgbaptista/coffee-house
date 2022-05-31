caae0058013cd409cbc8b7c142b70f2c8fa3ff0a
Robert <contentmediaguy@gmail.com>
2022-05-31 13:06:12 -0400

    Modified the background img for announcement and contact sections

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -4,22 +4,20 @@ export default function Announcement() {
     
     
     return (
-        <div className="bg-image">
-            <section id="announcement">
-                <div className="content">
-                    <div className="logo">
-                        <img src="https://user-images.githubusercontent.com/65358991/170405371-0e58feb1-7ae2-449f-861f-cbdc39c04f0b.png" alt="Coffee House Logo" />
-                    </div>
-                    <div className="message">
-                        <h1>Join us for our Grand Opening event!</h1>
-                        <h2>
-                            Come celebrate with us over a free cup of our House Blend and a chance to win FREE coffee for a year!
-                            <br /><br />
-                            123 Main St. | September 1-3
-                        </h2>
-                    </div>
+        <section id="announcement">
+            <div className="content">
+                <div className="logo">
+                    <img src="https://user-images.githubusercontent.com/65358991/170405371-0e58feb1-7ae2-449f-861f-cbdc39c04f0b.png" alt="Coffee House Logo" />
                 </div>
-            </section>
-        </div>
+                <div className="message">
+                    <h1>Join us for our Grand Opening event!</h1>
+                    <h2>
+                        Come celebrate with us over a free cup of our House Blend and a chance to win FREE coffee for a year!
+                        <br /><br />
+                        123 Main St. | September 1-3
+                    </h2>
+                </div>
+            </div>
+        </section>
     );
 }
